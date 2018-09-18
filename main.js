47fab2e95bf862531d596e27163c3bfbfa303844
Mohd Shahril <moharilstackup@gmail.com>
2018-09-18 14:41:29 +0800

    Initial setup

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ const app = express();
 //Step 3: define routes
 
 //Serves from public
+app.use(express.static(path.join(__dirname, "public")));
+/* app.use((req,res) => {
+    //Redirect the request to the file not found page
+    //Also in public directory
+    res.redirect("/");
+ }); */
 
 //Step 4: start the server
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
