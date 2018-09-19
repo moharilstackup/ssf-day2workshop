231b482ef1ff510e5aa0b08bd9a66d0e0d6cfe4b
moharilstackup <moharilstackup@gmail.com>
2018-09-19 13:35:12 +0800

    Chuk main.js

--- main.js
+++ main.js
@@ -1,49 +1,44 @@
-//Step 1: load path and express
-const express = require("express");
-const path = require("path");
-const resources = ['images', 'public'];
+//Load libs
+const path = require('path');
+const express = require('express');
 
-//Step 2: create an instance of the application
-const app = express();
+const resources = [ 'images', 'public' ];
+const images = [ 'bear.jpg', 'cat.jpg', 'dog.jpg' ]
 
-const images = ["cat.jpg", "dog.jpg", "bear.jpg"];
-const randimage = (array) => {
+const randImage = (array) => {
     const rand = Math.random();
-    const index = Math.floor(rand * array.length);
+    const index = Math.floor(rand * array.length)
     return (array[index]);
 }
 
-//Step 3: define routes
+//Create an instance of Express
+const app = express();
+
+//Define our routes
+// GET /image -> text/html
 app.get('/image', (req, resp) => {
-    //status
-    resp.status(203);
+    resp.status(200);
     resp.type('text/html');
-
-    var myPixList = new Array("cat.jpg", "dog.jpg", "bear.jpg");
-    var randomNum = (Math.floor(Math.random() * (myPixList.length)));
-
-    resp.send(`<h1>hello ${randomNum}</h1><img src=${myPixList[randomNum]} height="300" width="300">"`);
+    resp.send(`<img src='/${randImage(images)}'>`);
 });
 
+//GET /random-image -> image/png
 app.get('/random-image', (req, resp) => {
-    const imageFile= randimage(images);
+    const imageFile = randImage(images);
     resp.status(200);
-    resp.type('image/jpg');
+    resp.type('image/png');
     resp.sendfile(path.join(__dirname, 'images', imageFile));
 });
 
-//Serves from public
-// app.use(express.static(path.join(__dirname, "public")));
-// app.use(express.static(path.join(__dirname, "images")));
 for (let res of resources) {
-    console.info(`Adding ${res} to static`);
+    console.info(`Adding ${res} to static`)
     app.use(express.static(path.join(__dirname, res)));
 }
 
-//Step 4: start the server
-const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
+//Start the Express application
+const PORT = parseInt(process.argv[2]) || 
+        parseInt(process.env.APP_PORT) || 3000;
 
 app.listen(PORT, () => {
-    console.info(`Application started on port ${PORT} at ${new Date()}`);
-}
-);
+    console.info(`Application started on port ${PORT}`);
+});
