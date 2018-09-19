d5d7a9bd2a38885d8dcbf81ded8f6ba27be81bdd
moharilstackup <moharilstackup@gmail.com>
2018-09-19 16:01:49 +0800

    Include optional workshop asciify

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 //Load libs
 const path = require('path');
 const express = require('express');
+const asciify = require('asciify-image');
 
-const resources = [ 'images', 'public' ];
+var options = {
+    fit: 'box',
+    width: 100,
+    height: 50
+}
+
+
+const resources = ['images', 'public'];
 // const images = [ 'bear.jpg', 'cat.jpg', 'dog.jpg','th8.png','th9.png','th10.png', 'th11.png','th12.png' ];
-const images = [ 'th7.png','th8.png','th9.png','th10.png', 'th11.png','th12.png' ]
+const images = ['th7.png', 'th8.png', 'th9.png', 'th10.png', 'th11.png', 'th12.png']
 
 const randImage = (array) => {
     const rand = Math.random();
     const index = Math.floor(rand * array.length)
     return (array[index]);
 }
+
+
 
 //Create an instance of Express
 const app = express();
@@ -27,8 +37,21 @@ app.get('/image', (req, resp) => {
         'image/png': () => {
             resp.sendfile(path.join(__dirname, 'images', imageFile));
         },
-        'application/json': () => {     
-            resp.json({filename: imageFile});
+        'application/json': () => {
+            resp.json({ filename: imageFile });
+        },
+        'text/plain': () => {
+            asciify(path.join(__dirname, 'images', imageFile), options)
+                .then(function (asciified) {
+                    // Print asciified image to console
+                    console.log(asciified);
+                    resp.send(asciified);
+                })
+                .catch(function (err) {
+                    // Print error to console
+                    console.error(err);
+                });
+            // resp.send("Test");
         },
         'default': () => {
             resp.status(406);
@@ -37,30 +60,14 @@ app.get('/image', (req, resp) => {
     });
 });
 
-// -------------- to be commented
-/* app.get('/image', (req, resp) => {
-    resp.status(200);
-    resp.type('text/html');
-    resp.send(`<img src='/${randImage(images)}'>`);
-});
-
-//GET /random-image -> image/png
-app.get('/random-image', (req, resp) => {
-    const imageFile = randImage(images);
-    resp.status(200);
-    resp.type('image/png');
-    resp.sendfile(path.join(__dirname, 'images', imageFile));
-}); */
-// -------------- to be commented
-
 for (let res of resources) {
     console.info(`Adding ${res} to static`)
     app.use(express.static(path.join(__dirname, res)));
 }
 
 //Start the Express application
-const PORT = parseInt(process.argv[2]) || 
-        parseInt(process.env.APP_PORT) || 3000;
+const PORT = parseInt(process.argv[2]) ||
+    parseInt(process.env.APP_PORT) || 3000;
 
 app.listen(PORT, () => {
     console.info(`Application started on port ${PORT}`);
